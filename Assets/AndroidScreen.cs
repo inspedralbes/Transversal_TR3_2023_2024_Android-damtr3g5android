7cf1d0695b4ea742d6cd505559ef52fecc4cbc15
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-03-13 12:58:39 +0100

    Creado joystick tactil y posicion

---
+++ Assets/AndroidScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeRight;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterController : MonoBehaviour
+    {
+        public float speed;
+
+        public float speedJoystick;
+
+        private Animator animator;
+        float horizontalMove = 0;
+        float verticallMove = 0;
+        private Rigidbody2D rigidBody;
+        public Joystick joystick;
+        private bool isColliding = false;
+
+
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+
+        private void Update()
+        {
+            Vector2 dir = Vector2.zero;
+
+            
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                dir.x = -1;
+                animator.SetInteger("Direction", 3);
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                dir.x = 1;
+                animator.SetInteger("Direction", 2);
+            }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                dir.y = 1;
+                animator.SetInteger("Direction", 1);
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                dir.y = -1;
+                animator.SetInteger("Direction", 0);
+            }
+
+            dir.Normalize();
+            animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            if (dir!=Vector2.zero) {
+                GetComponent<Rigidbody2D>().velocity = speed * dir;
+            }
+            else {
+                horizontalMove = joystick.Horizontal * speed;
+                verticallMove = joystick.Vertical * speed;
+
+                if (horizontalMove<0)
+                {
+                    
+                    animator.SetInteger("Direction", 3);
+                }
+                else if (horizontalMove > 0)
+                {
+                    
+                    animator.SetInteger("Direction", 2);
+                }
+
+                if (verticallMove > 0 && Mathf.Abs(verticallMove)> Mathf.Abs(horizontalMove))
+                {
+                    
+                    animator.SetInteger("Direction", 1);
+                }
+                else if (verticallMove < 0 && Mathf.Abs(verticallMove) > Mathf.Abs(horizontalMove))
+                {
+                    
+                    animator.SetInteger("Direction", 0);
+                }
+
+
+
+
+                GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove, verticallMove).normalized * speedJoystick;
+            }
+
+            
+
+
+            //transform.position += new Vector3(horizontalMove, verticallMove, 0) * Time.deltaTime * speedJoystick;
+
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            isColliding = true;
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            isColliding = false;
+        }
+
+
+        
+    }
+}
