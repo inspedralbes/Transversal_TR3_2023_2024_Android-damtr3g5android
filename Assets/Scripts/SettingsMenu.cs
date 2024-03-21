b8193ae0cd678a68d5a1fa0b25c5d6e731ee07a0
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-03-21 10:38:44 +0100

    Creada pruebas de logica para desactivar musica

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -9,6 +9,9 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField] public Slider volumenSonidos;
     [SerializeField] public GameObject[] sonidos;
     [SerializeField] public GameObject[] musica;
+    private float ultimoVolumenMusica;
+    private float ultimoVolumenSonidos;
+
 
     private void Start()
     {
@@ -35,15 +38,42 @@ public class SettingsMenu : MonoBehaviour
     }
 
     public void GuardarVolumenMusica(){
-        //Seteamos el volumen de la musica segun su valor en las opciones
-        PlayerPrefs.SetFloat("volumenMusica", volumenMusica.value);
+        
+            //Seteamos el volumen de la musica segun su valor en las opciones
+            PlayerPrefs.SetFloat("volumenMusica", volumenMusica.value);
+       
+        
     }
 
     public void GuardarVolumenSonidos()
     {
-        //Seteamos el volumen de los sonidos segun su valor en las opciones
-        PlayerPrefs.SetFloat("volumenSonidos", volumenSonidos.value);
+        
+            //Seteamos el volumen de los sonidos segun su valor en las opciones
+            PlayerPrefs.SetFloat("volumenSonidos", volumenSonidos.value);
+        
+        
     }
 
-    
+    public void MuteVolumenMusica(bool musica)
+    {
+        ultimoVolumenMusica = PlayerPrefs.GetFloat("volumenMusica");
+        if (musica) {
+            PlayerPrefs.SetFloat("volumenMusica", 0);
+        }
+        else
+            PlayerPrefs.SetFloat("volumenMusica", ultimoVolumenMusica);
+    }
+
+    public void MuteVolumenSonidos(bool sonido)
+    {
+        ultimoVolumenSonidos = PlayerPrefs.GetFloat("volumenSonidos");
+        if (sonido)
+        {
+            PlayerPrefs.SetFloat("volumenSonidos", 0);
+        }
+        else
+            PlayerPrefs.SetFloat("volumenSonidos", ultimoVolumenSonidos);
+    }
+
+
 }
